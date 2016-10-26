cffe3c44505fc6ec9f29845a0bc4b654c2efa39e
Rafa Ramirez <rapharl85@gmail.com>
2016-10-25 21:07:56 -0700

    added spinner to load stocks, ajax and form

---
+++ db/migrate/20161022233316_fix_colum_name_stock.rb
@@ -0,0 +1,5 @@
+class FixColumNameStock < ActiveRecord::Migration[5.0]
+  def change
+    rename_column :stocks, :ticket, :ticker
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161022225625) do
+ActiveRecord::Schema.define(version: 20161022233316) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "stocks", force: :cascade do |t|
-    t.string   "ticket"
+    t.string   "ticker"
     t.string   "name"
     t.decimal  "last_price"
     t.datetime "created_at", null: false
